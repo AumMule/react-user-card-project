b7ed9204d30f8fcd05bc71a3dabcda4606178893
AumMule <ommule2005@gmail.com>
2025-07-31 17:29:40 +0530

    added search feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Usercard from './components/Usercard';
 import DarkModeToggle from './components/DarkModeToggle';
+import { useState } from 'react';
+
 function App() {
+
+  const [search, setSearch] = useState("");
+
   const users = [
     {
       name: "Aum Mule",
@@ -19,16 +24,29 @@ function App() {
     }
   ];
 
+  const filterUsers = users.filter(user=>
+    user.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="bg-gray-100 min-h-screen py-10 px-5 dark:bg-gray-900 dark:text-white transition-colors duration-300">
     {/* Top bar for dark mode toggle */}
-    <div className="flex justify-end mb-6">
-      <DarkModeToggle />
-    </div>
+    <div className="flex flex-col sm:flex-row justify-between items-center mb-6 gap-4">
+        <DarkModeToggle />
+        
+        {/* Search input */}
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={ search }
+          onChange={(e) => setSearch(e.target.value)}
+          className="px-4 py-2 rounded-md border border-gray-300 dark:border-gray-700 dark:bg-gray-800 focus:outline-none dark:text-white focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
 
     {/* Grid of user cards */}
     <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-4">
-      {users.map((user, index) => (
+      {filterUsers.map((user, index) => (
         <Usercard
           key={index}
           name={user.name}
